62aaabc145dc21d66285ba07817da99aecd8681b
Kyle Zimmer II <24055429@nwtc.edu>
2020-12-12 08:36:14 -0600

    About page CMS preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { AboutPageTemplate } from '../../templates/about-page';
+import React from "react";
+import PropTypes from "prop-types";
+import { AboutPageTemplate } from "../../templates/about-page";
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
-);
+const AboutPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(["data"]).toJS();
+
+  if (data) {
+    return (
+      <AboutPageTemplate
+        title={data.title}
+        headerImage={data.headerImage}
+        storyImage={data.storyImage}
+        content={widgetFor("body")}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
